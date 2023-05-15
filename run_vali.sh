c7affca20f4172a129edea11438e54c58c789b7e
sung.balance <sung.balance@gmail.com>
2023-05-15 03:15:07 +0000

    update script

--- run_vali.sh
+++ run_vali.sh
@@ -1,19 +1,11 @@
-sudo python dialogues/risawoz/src/convert.py --src en --tgt kr --setting kr --splits valid --debug \
-python dialogues/risawoz/src/convert.py --src en --tgt kr --setting kr --splits fewshot --debug \
-python dialogues/risawoz/src/convert.py --src en --tgt kr --setting kr --splits test --debug
+for split in test valid train fewshot ; do
+    python dialogues/risawoz/src/convert.py --src en --tgt kr --setting kr --splits ${split} --debug
+done
 
 
-python dialogues/risawoz/src/preprocess.py --max_history 2 --last_two_agent_turns --gen_full_state --only_user_rg --sampling balanced --setting kr --fewshot_percent 0 --version 1 --splits valid \
-
-python dialogues/risawoz/src/preprocess.py --max_history 2 --last_two_agent_turns --gen_full_state --only_user_rg --sampling balanced --setting kr --fewshot_percent 0 --version 1 --splits fewshot \
-
-python dialogues/risawoz/src/preprocess.py --max_history 2 --last_two_agent_turns --gen_full_state --only_user_rg --sampling balanced --setting kr --fewshot_percent 0 --version 1 --splits test
-
-
-
-
-
-# run all tests
+for split in test valid train fewshot ; do
+    python dialogues/risawoz/src/preprocess.py --max_history 2 --last_two_agent_turns --gen_full_state --only_user_rg --sampling balanced --setting kr --fewshot_percent 0 --version 1 --splits ${split}
+done
 
 for split in test valid train fewshot ; do
     python3 dialogues/risawoz/scripts/check_entity.py --directory dialogues/risawoz/data/preprocessed/ --setting kr --version 1 --splits ${split}
